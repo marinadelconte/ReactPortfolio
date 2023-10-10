105467bf51cdc5ae353f73387e8dc07a05f15e9c
Marina Del Conte <marinadelconte@yahoo.com>
2023-10-09 20:10:09 -0400

    adding mapping component to cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,14 @@
 import '../styles/styles.css';
 
-function Project() {
+function Project(props) {
   return (
     <div>
-      <div className="card" style={{width: "300px"}}>
+      <div className="card" style={{width: "300px", height: "250px"}}>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="card-link">Card link</a>
+          <h5 className="card-title">{props.project.projectTitle}</h5>
+          <h6 className="card-subtitle mb-2">{props.project.projectUrl}</h6>
+          <p className="card-text">{props.project.about}</p>
+          <a href="#" className="card-link">View Project</a>
         </div>
       </div>
     </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,17 +1,47 @@
 import Project from '/src/components/Project';
 import '../styles/styles.css';
 
-
+const projects = [
+{
+  projectTitle: "test",
+  projectUrl: "www.google.com",
+  about: "asdfsdf"
+},
+{
+  projectTitle: "Travel Tracker",
+  projectUrl: "www.yahoo.com",
+  about: "asdfsdf"
+},
+{
+  projectTitle: "Brewery Locator & Random Joke Generator",
+  projectUrl: "https://pb1983.github.io/Project1/",
+  about: "asdfsdf"
+},
+{
+  projectTitle: "test2",
+  projectUrl: "www.yahoo.com",
+  about: "asdfsdf"
+},
+{
+  projectTitle: "test2",
+  projectUrl: "www.yahoo.com",
+  about: "asdfsdf"
+},
+{
+  projectTitle: "test2",
+  projectUrl: "www.yahoo.com",
+  about: "asdfsdf"
+}
+]
 
 export default function Portfolio() {
     return (
       <div>
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
+          {projects.map((project, id) => (
+            <Project project={project} key={"project" + id}>
+
+            </Project>
+          ))}
       </div>
     );
   }
