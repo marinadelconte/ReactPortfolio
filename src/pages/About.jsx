d9ed001be2d415012e99d625062add672f8d37e7
Marina Del Conte <marinadelconte@yahoo.com>
2023-10-17 00:01:55 -0400

    edited font size on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
       <div>
       <h1>Hi there!</h1>
-      <p>Welcome to my digital portfolio! I'm glad you decided to stop by! I am a Full Stack Software Developer that is building experience creating web applications like the very one you are on! Click on any of the tabs above to see examples of my work, resume, and how to contact me. Here are some fast facts about me to get to know me better: </p>
+      <p id="about">Welcome to my digital portfolio! I'm glad you decided to stop by! I am a Full Stack Software Developer that is building experience creating web applications like the very one you are on! Click on any of the tabs above to see examples of my work, resume, and how to contact me. Here are some fast facts about me to get to know me better: </p>
                     <ul>
                         <li>Age: 28</li>
                         <li>Location: Philadelphia, Pa</li>
