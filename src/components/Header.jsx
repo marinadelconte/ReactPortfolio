911338e52e3b253e9819e0caa5f931c9171da965
Marina Del Conte <marinadelconte@yahoo.com>
2023-10-18 13:39:15 -0400

    added logo to header with a link that takes you back to the homepage when clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,9 @@ import '../styles/styles.css';
 function Header() {
     return (
       <header>
-        <h1 className="header">Marina Del Conte</h1>
-        <h2 className="subheader">Full Stack Software Developer</h2>
+        <a href="/"><img className="logo" src="./initials.PNG" alt="logo"></img></a>
+        {/* <h1 className="header">Marina Del Conte</h1>
+        <h2 className="subheader">Full Stack Software Developer</h2> */}
       </header>
     );
   }
