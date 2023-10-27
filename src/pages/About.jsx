28d4f38478333bc0ed0d52c729676e84c1830877
Marina Del Conte <marinadelconte@yahoo.com>
2023-10-27 10:20:33 -0400

    fixed grammatical error on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <>
       <div className="about-text">
         <h2>Hi, thanks for stopping by.</h2>
-          <p>My name is Marina Del Conte and I am Full Stack Software Developer who enjoys experimenting and creating fun, interactive designs through code. I am a recent graduate of the University of Pennsylvania's Full Stack Coding Bootcamp, and prior to enrolling in the Bootcamp, I was working in financial marketing as a Communications Coordinator. After much consideration, I decided to change career paths and pursue a career in software development. I am continuing to grow and expand upon my experience, and look forward to career opportunities to succeed as a Full Stack Software Developer. </p>
+          <p>My name is Marina Del Conte and I am a Full Stack Software Developer who enjoys experimenting and creating fun, interactive designs through code. I am a recent graduate of the University of Pennsylvania's Full Stack Coding Bootcamp, and prior to enrolling in the Bootcamp, I was working in financial marketing as a Communications Coordinator. After much consideration, I decided to change career paths and pursue a career in software development. I am continuing to grow and expand upon my experience, and looking forward to career opportunities to succeed as a Full Stack Software Developer. </p>
       </div>
       <div className="social-media-logos">
         <h3>View more of my professional experience here: </h3>
