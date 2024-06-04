c1b8b5179082efa66ca5bc2c2368ad63f765fd11
Marina Del Conte <marinadelconte@yahoo.com>
2024-06-04 19:37:51 -0400

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/styles.css';
 function Footer() {
     return (
       <footer className="footer-container">
-      <p className="footer">Made with 💜 By Marina |<a href="https://www.vecteezy.com/free-vector/linkedin-logo" target="_blank"> Linkedin Logo Vectors by Vecteezy</a></p> 
+      <p className="footer">Made By Marina |<a href="https://www.vecteezy.com/free-vector/linkedin-logo" target="_blank"> Linkedin Logo Vectors by Vecteezy</a></p> 
       
       </footer>
     );
